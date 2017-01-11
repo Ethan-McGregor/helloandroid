842ef8f58065624e9a5ac6569c7e8ceccaf7327c
Ethan-McGregor <ethanm4@uw.edu>
2017-01-11 13:16:36 -0800

    addded the android hello project

---
+++ hello/app/src/main/java/hello/ethanm4/washington/edu/hello/helloAndroid.java
@@ -0,0 +1,13 @@
+package hello.ethanm4.washington.edu.hello;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class helloAndroid extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hello_android);
+    }
+}
